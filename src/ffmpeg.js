e44011d95c58f8cf70d9c65f9d61310f2715a01e
Vedant Nimbarte <nimbartevedant@gmail.com>
2024-05-24 21:50:23 +0530

    updated ffmpeg command

--- src/ffmpeg.js
+++ src/ffmpeg.js
@@ -26,22 +26,24 @@ const convertVideoToGif = async (ffmpeg) => {
   //   "gif",
   //   "output.gif",
   // ]);
-  const res = await ffmpeg.exec([
+  await ffmpeg.exec([
     "-i",
     "input.webm",
     "-vf",
-    "fps=10,scale=1920:1080:flags=lanczos,palettegen",
+    "fps=15,scale=1920:1080:flags=lanczos,palettegen=max_colors=256",
+    // "fps=10,scale=1920:1080:flags=lanczos,palettegen",
     // "-y",
     "palette.png",
   ]);
-  console.log("palette.png", await ffmpeg.listDir("."), res);
+  console.log("palette.png", await ffmpeg.listDir("."));
   await ffmpeg.exec([
     "-i",
     "input.webm",
     "-i",
     "palette.png",
     "-filter_complex",
-    "fps=60,scale=1920:1080:flags=lanczos[x];[x][1:v]paletteuse,setpts=2*PTS",
+    // "fps=60,scale=1920:1080:flags=lanczos[x];[x][1:v]paletteuse,setpts=2*PTS",
+    "fps=15,scale=2560:-1:flags=lanczos[x];[x][1:v]paletteuse=dither=bayer:bayer_scale=5,setpts=2*PTS",
     "-c:v",
     "gif",
     "output.gif",
