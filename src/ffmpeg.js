1474086df71179a39d9387b3769cdc25ecdefc5f
Vedant Nimbarte <nimbartevedant@gmail.com>
2024-05-25 23:30:17 +0530

    removed unused code

--- src/ffmpeg.js
+++ src/ffmpeg.js
@@ -1,55 +1,41 @@
 // ffmpegUtils.js
 import { FFmpeg } from "@ffmpeg/ffmpeg";
 import { fetchFile, toBlobURL } from "@ffmpeg/util";
+import { FPS, MIMETYPES, RESOLUTION } from "./utils/constants";
 
 const loadFFmpeg = async () => {
   const baseURL = "https://unpkg.com/@ffmpeg/core@0.12.6/dist/umd";
   const ffmpeg = new FFmpeg();
   await ffmpeg.load({
-    coreURL: await toBlobURL(`${baseURL}/ffmpeg-core.js`, "text/javascript"),
-    wasmURL: await toBlobURL(`${baseURL}/ffmpeg-core.wasm`, "application/wasm"),
+    coreURL: await toBlobURL(`${baseURL}/ffmpeg-core.js`, MIMETYPES.json),
+    wasmURL: await toBlobURL(`${baseURL}/ffmpeg-core.wasm`, MIMETYPES.wasm),
   });
+  window.ffmpeg = ffmpeg;
   return ffmpeg;
 };
 
-// ffmpeg -i input_video.mp4 -vf "fps=15,scale=320:240:flags=lanczos" -gifflags +transdiff -y output.gif
-
 const convertVideoToGif = async (ffmpeg) => {
-  // await ffmpeg.exec([
-  //   "-i",
-  //   "input.webm",
-  //   "-vf",
-  //   "fps=60,scale=1920:1080,setpts=2*PTS",
-  //   "-loop",
-  //   "0",
-  //   "-c:v",
-  //   "gif",
-  //   "output.gif",
-  // ]);
   await ffmpeg.exec([
     "-i",
     "input.webm",
     "-vf",
-    "fps=15,scale=1920:1080:flags=lanczos,palettegen=max_colors=256",
-    // "fps=10,scale=1920:1080:flags=lanczos,palettegen",
-    // "-y",
+    `fps=${FPS},scale=${RESOLUTION.width}:${RESOLUTION.height}:flags=lanczos,palettegen=max_colors=256`,
     "palette.png",
   ]);
-  console.log("palette.png", await ffmpeg.listDir("."));
+
   await ffmpeg.exec([
     "-i",
     "input.webm",
     "-i",
     "palette.png",
     "-filter_complex",
-    // "fps=60,scale=1920:1080:flags=lanczos[x];[x][1:v]paletteuse,setpts=2*PTS",
-    "fps=15,scale=2560:-1:flags=lanczos[x];[x][1:v]paletteuse=dither=bayer:bayer_scale=5,setpts=2*PTS",
+    `scale=${RESOLUTION.width}:${RESOLUTION.height}:flags=lanczos[x];[x][1:v]paletteuse`,
     "-c:v",
     "gif",
     "output.gif",
   ]);
   const data = await ffmpeg.readFile("output.gif");
-  return URL.createObjectURL(new Blob([data.buffer], { type: "image/gif" }));
+  return URL.createObjectURL(new Blob([data.buffer], { type: MIMETYPES.gif }));
 };
 
 export { loadFFmpeg, convertVideoToGif, fetchFile };
